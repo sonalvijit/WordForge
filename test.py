f9b305a9a9f0f2a9fccff46b392f6c778685ecd8
svjt <josephtoppo.sxd@gmail.com>
2025-05-22 00:14:08 +0530

    Removed Unwanted File

--- test.py
+++
@@ -1,5 +0,0 @@
-from server import load_existing_data
-
-a = load_existing_data()
-for i in a:
-    print(i)
--- utils.py
+++
@@ -1,7 +0,0 @@
-from server import load_existing_data
-
-def initial_db_state():
-     a = load_existing_data()
-     print(a[1])
-
-initial_db_state()
